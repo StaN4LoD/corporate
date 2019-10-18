eee4abba0f2d2a2e9f324c19f1851e48e10a9cdd
StaN4LoD <stan4lod@yandex.ru>
2019-10-18 21:06:41 +0300

    Маршруты и Контроллеры пользовательской части

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    //
+
+    protected $p_rep;
+    protected $s_rep;
+    protected $a_rep;
+    protected $m_rep;
+
+    protected $template;
+
+    protected $vars;
+
+    protected $contentRightBar = FALSE;
+    protected $contentLeftBAr = FALSE;
+
+    protected $bar = FALSE;
+
+
+    public function __construct () {
+
+    }
+
+}
